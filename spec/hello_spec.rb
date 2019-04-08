0657b391dbd879ce4da12f1bc690481434d62269
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:36:29 +0000

    Mon Apr 08 10:36:29 UTC 2019

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -2,10 +2,13 @@ require './lib/solutions/HLO/hello'
 
 describe Hello do
 
+  let (:greet) { Hello.new }
+
   it "says Hello to the world" do
-    expect(hello("Luke")).to eq("Hello Luke")
+    expect(greet.hello("Luke")).to eq("Hello Luke")
   end
 
 end
+
 
 
--- spec/sum_spec.rb
+++ spec/sum_spec.rb
@@ -2,10 +2,13 @@ require './lib/solutions/SUM/sum'
 
 describe Sum do
 
+  let (:calculator) { Sum.new }
+
   it "adds two numbers together" do
-    expect(sum(1,2)).to eq(3)
+    expect(calculator.sum(1,2)).to eq(3)
   end
 
 end
+
 
 
