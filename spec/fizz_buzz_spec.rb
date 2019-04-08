9aa084907e934dd72506222f266319a7f8bb2dec
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:48:14 +0000

    Mon Apr 08 10:48:14 UTC 2019

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,8 +2,11 @@ require './lib/solutions/FIZ/fizz_buzz.rb'
 
 describe Fizzbuzz do
 
-  it "returns 'Fizz' for multiples of 3" do
-    
+  let (:fb) { FizzBuzz.new }
+
+  it "returns 'fizz' for multiples of 3" do
+    expect(fb.fizz_buzz(3)).to eq('fizz')
   end
 
 end
+
