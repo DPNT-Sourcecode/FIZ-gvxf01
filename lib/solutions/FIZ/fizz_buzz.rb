a2964f3ac0f2283cff02b3554fc036b95a61dc67
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:59:06 +0000

    Mon Apr 08 13:59:06 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -7,8 +7,8 @@ class FizzBuzz
     thing = number.to_s.chars
     answer.push('fizz') if number % 3 == 0 || thing.include?("3")
     answer.push('buzz') if number % 5 == 0 || thing.include?("5")
-    answer.push('deluxe') if number % 2 == 0 && (number % 3 == 0 && thing.include?("3"))
-    answer.push('fake deluxe') if number % 2 != 0 && number > 10 && thing.uniq.length == 1
+    answer.push('deluxe') if number % 2 == 0 && (number % 3 == 0 && thing.include?("3")) || (number % 5 == 0 && thing.include?("5"))
+    answer.push('fake deluxe') if number % 2 != 0 && (number % 3 == 0 && thing.include?("3")) || (number % 5 == 0 && thing.include?("5")) 
 
 
     return number if answer.empty?
@@ -29,5 +29,6 @@ class FizzBuzz
   end
 
 end
+
 
 
