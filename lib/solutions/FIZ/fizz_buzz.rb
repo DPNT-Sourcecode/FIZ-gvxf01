e237872f7c3c327eed71e7f032437a760b834550
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 11:55:11 +0000

    Mon Apr 08 11:55:11 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,9 +3,14 @@ class FizzBuzz
 
   def fizz_buzz(number)
 
-    string_number = number.to_s
-    string_number.include? "3" ? number % 5 == 0 ? 'fizz buzz' : 'buzz'
-    string_number.include? "5" ? number % 3 == 0 ? 'fizz buzz' : 'fizz'
+    if number.to_s.include? "3"
+      thingy = 1
+    elsif number.to_s.include? "5"
+      thingy = 2
+    end
+
+    thingy == 1 ? number % 5 == 0 ? 'fizz buzz' : 'buzz'
+    thingy == 2 ? number % 3 == 0 ? 'fizz buzz' : 'fizz'
     number
 
     # return 'fizz' if number.to_s.include? "3" || number % 3 == 0
@@ -15,7 +20,3 @@ class FizzBuzz
   end
 
 end
-
-
-
-
