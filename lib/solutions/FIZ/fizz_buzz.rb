cb68745d926ab829eb8a624f3ed408024f9f7ade
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 12:01:11 +0000

    Mon Apr 08 12:01:11 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,8 +3,11 @@ class FizzBuzz
 
   def fizz_buzz(number)
 
-    number.to_s.include? "3" ? number % 5 == 0 ? return 'fizz buzz' : return 'buzz'
-    number.to_s.include? "5" ? number % 3 == 0 ? return 'fizz buzz' : return 'fizz'
+    fizz = "fizz"
+    buzz = "buzz"
+    fizzbuzz = "fizz buzz"
+    number.to_s.include? "3" ? number % 5 == 0 ? return fizz_buzz : return buzz
+    number.to_s.include? "5" ? number % 3 == 0 ? return fizz_buzz : return fizz
     return 'fizz' if number % 3 == 0
     return 'buzz' if number % 5 == 0
 
@@ -17,4 +20,5 @@ class FizzBuzz
   end
 
 end
+
 
