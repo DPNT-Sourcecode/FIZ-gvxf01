df29fb2e028e3bf9eea27613a977bd03a425c7fe
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 12:59:18 +0000

    Mon Apr 08 12:59:18 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,13 +3,22 @@ class FizzBuzz
 
   def fizz_buzz(number)
 
+    answer = []
     thing = number.to_s.chars
-    return 'fizz buzz' if thing.include?("3") && thing.include?("5") || thing.include?("3") && number % 5 == 0 || thing.include?("5") && number % 3 == 0 || number % 3 == 0 && number % 5 == 0
-    return 'fizz' if number % 3 == 0 || thing.include?("3")
-    return 'buzz' if number % 5 == 0 || thing.include?("5")
-    number
+    answer.push('fizz') if number % 3 == 0 || thing.include?("3")
+    answer.push('buzz') if number % 5 == 0 || thing.include?("5")
+    answer.push('deluxe') if number > 10
+    answer.join(" ")
+
+
+    # thing = number.to_s.chars
+    # return 'fizz buzz' if thing.include?("3") && thing.include?("5") || thing.include?("3") && number % 5 == 0 || thing.include?("5") && number % 3 == 0 || number % 3 == 0 && number % 5 == 0
+    # return 'fizz' if number % 3 == 0 || thing.include?("3")
+    # return 'buzz' if number % 5 == 0 || thing.include?("5")
+    # number
 
   end
 
 end
+
 
